323b2bbe402138cce9ecbb82bafab9569df9acd1
surat pyari <suratpyari.db21@gmail.com>
2019-05-26 17:43:49 +0530

    Missing js files added

--- app/assets/javascripts/spree/backend/spree_flexi_variants.js
+++ app/assets/javascripts/spree/backend/spree_flexi_variants.js
@@ -1,4 +1,4 @@
 //= require jquery.formatCurrency-1.4.0.min
-//= require i18n/jquery.formatCurrency.all
-//= require jquery.validate/jquery.validate.min
+//= require jquery.formatCurrency.all
+//= require jquery.validate
 //= require_tree
